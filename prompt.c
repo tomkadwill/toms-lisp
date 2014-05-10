87835fe45cf28effc945cf494c5ec49fdef65603
Tom Kadwill <tomkadwill@gmail.com>
2014-05-10 20:58:57 +0100

    Added preprocessor to determine whether windows or unix

--- prompt.c
+++ prompt.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a static buffet for user input of maximum size 2048 */
-static char input[2048];
+/* if we are compiling on windows */
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+
+/* fake readline function */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* fake add_history function */
+void add_history(char* unused) {}
+
+/* otherwise include the editline headers */
+#else
+
+#include <editline/readline.h>
+
+#endif
 
 int main(int argc, char** argv) {
 
@@ -13,13 +38,12 @@ int main(int argc, char** argv) {
   while(1) {
 
     /* Output or prompt */
-    fputs("toms-lisp> ", stdout);
+    char* input = readline("toms-lisp> ");
+    add_history(input);
 
-    /* Read a line of user input of max size 2048 */
-    fgets(input, 2048, stdin);
-
-    /* Echo input back to the user */
     printf("No you're a %s", input);
+    free(input);
+
   }
 
   return 0;
