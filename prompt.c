a47ec988ae1fd878219d32e9de9cc1ec71b0c8ba
Tom Kadwill <tomkadwill@gmail.com>
2014-05-14 08:57:28 +0100

    updated regex to accept integers and decimals

--- prompt.c
+++ prompt.c
@@ -40,7 +40,7 @@ int main(int argc, char** argv) {
   /* Define them with the following Language */
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                     \
-      number   : /-?[0-9]+/ ;                             \
+      number   : /[0-9]+(\\.[0-9][0-9]?)?/ ;                             \
       operator : '+' | '-' | '*' | '/' | '%' | /add/ | /sub/ | /mul/ | /div/ ;    \
       expr     : <number> | '(' <operator> <expr>+ ')' ;  \
       lispy    : /^/ <operator> <expr>+ /$/ ;             \
